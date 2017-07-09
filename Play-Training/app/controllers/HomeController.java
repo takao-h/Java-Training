c906d3f2c06ff55cd439563cbdd4eb4755dbba6e
takao-h <hayashi_takao@cyberbuzz.co.jp>
2017-07-09 22:22:25 +0900

    Add chat method

--- Play-Training/app/controllers/HomeController.java
+++ Play-Training/app/controllers/HomeController.java
@@ -1,8 +1,18 @@
 package controllers;
 
-import play.mvc.*;
-
-import views.html.*;
+import actors.ChatRoomActor;
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.contrib.process.Sink;
+import akka.stream.javadsl.Flow;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.reactivestreams.Publisher;
+import play.mvc.Controller;
+import play.mvc.Http;
+import play.mvc.Result;
+import play.mvc.WebSocket;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -18,6 +28,10 @@ public class HomeController extends Controller {
      */
     public Result index() {
 
+        ActorSystem actorSystem = ActorSystem.create();
+        ActorRef chatRoomActor = actorSystem.actorOf(Props.create(ChatRoomActor.class));
+        static final Publisher<JsonNode> publisher = new Publisher<>();
+
 
 
         return ok(index.render("Your new application is ready."));
@@ -25,8 +39,10 @@ public class HomeController extends Controller {
 
 
     public WebSocket ws(){
-        return WebSocket.Json.accept(requestHeader -> {
-           Flow<JsonNode, JsonNode, NotUsed> flow = Flow.create();
+        return WebSocket.Json.accept((Http.RequestHeader requestHeader) -> {
+            akka.stream.javadsl.Source<JsonNode, ?> source = publisher.register();
+            akka.stream.javadsl.Sink<JsonNode, NotUsed> sink = Sink.actorRef(chatRoomeActor, "Sucsess");
+            Flow<JsonNode, JsonNode, NotUsed> flow = Flow.fromSinkAndSource(sink, source);
            return flow;
         });
     }
